276703666f1556624b550c731fb98e8b0ab08c8e
Jacek Galowicz <jacek@galowicz.de>
2016-10-30 18:44:11 +0100

    Add a little unit test app

---
+++ test/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cassert>
+#include <cstring>
+#include <pprintpp.hpp>
+
+#define TEST(correct_str, ...) \
+    assert(!strcmp(AUTOFORMAT(__VA_ARGS__), correct_str))
+
+int main()
+{
+    TEST("", "");
+
+    TEST("%p", "{}", nullptr);
+    TEST("%p", "{}", reinterpret_cast<void*>(0));
+
+    // For safety reasons:
+    // Only print strings as strings, if the user also writes {s}
+    TEST("%p", "{}", "str");
+    TEST("%s", "{s}", "str");
+
+    TEST("%c",   "{}", static_cast<char>(123));
+
+    TEST("%d",   "{}", static_cast<short>(123));
+    TEST("%d",   "{}", 123);
+    TEST("%ld",  "{}", 123l);
+    TEST("%lld", "{}", 123ll);
+
+    TEST("%u",   "{}", 123u);
+    TEST("%lu",  "{}", 123ul);
+    TEST("%llu", "{}", 123ull);
+
+    TEST("%f",  "{}", 1.0f);
+    TEST("%lf", "{}", 1.0);
+
+    std::cout << "Green, green, green! All tests passed." << std::endl;
+    return 0;
+}
