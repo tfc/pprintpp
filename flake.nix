6cb2a94a923b6d14f9b0e4f58e8c66c34308e3c8
Jacek Galowicz <jacek@galowicz.de>
2022-04-13 10:39:20 +0200

    Transition to flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/master;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.pprintpp = pkgs.callPackage ./nix/build.nix { };
+        checks = {
+          clang-build = self.packages.${system}.pprintpp.override {
+            stdenv = pkgs.clangStdenv;
+          };
+          gcc-build = self.packages.${system}.pprintpp.override {
+            stdenv = pkgs.clangStdenv;
+          };
+        };
+        defaultPackage = self.packages.${system}.pprintpp;
+      }
+    )) // {
+      overlay = final: prev: {
+        pprintpp = final.callPackage ./nix/build.nix { };
+      };
+    };
+}
