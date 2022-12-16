b584f744bca0bab13f79b8f8c359f8ce8871bf1e
Jacek Galowicz <jacek@galowicz.de>
2022-12-16 23:06:22 +0100

    Add pre-commit-check and direnv

--- flake.nix
+++ flake.nix
@@ -1,15 +1,23 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
-  outputs = { self, nixpkgs, flake-parts }:
+  outputs = { self, flake-parts, nixpkgs, pre-commit-hooks }:
     flake-parts.lib.mkFlake { inherit self; }
       {
         systems = [ "x86_64-linux" ];
         perSystem = { config, pkgs, system, ... }: {
           packages.pprintpp = pkgs.callPackage ./nix/build.nix { };
           packages.default = config.packages.pprintpp;
+
+          devShells.default = pkgs.mkShell {
+            inputsFor = [ config.packages.pprintpp ];
+            shellHook = ''
+              ${config.checks.pre-commit-check.shellHook}
+            '';
+          };
           checks = {
             clang-build = config.packages.pprintpp.override {
               stdenv = pkgs.clangStdenv;
@@ -22,6 +30,13 @@
               src = ./example;
               buildInputs = [ config.packages.pprintpp ];
               nativeBuildInputs = [ pkgs.cmake ];
+            };
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                nixpkgs-fmt.enable = true;
+                statix.enable = true;
+              };
             };
           };
         };
