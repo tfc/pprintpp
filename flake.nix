be067a009fe44c3a289e36b77f42a076913f4b4b
Jacek Galowicz <jacek@galowicz.de>
2022-12-16 23:00:56 +0100

    Update inputs and use flake-parts instead of flake-utils

--- flake.nix
+++ flake.nix
@@ -1,32 +1,31 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/master;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
+  outputs = { self, nixpkgs, flake-parts }:
+    flake-parts.lib.mkFlake { inherit self; }
       {
-        packages.pprintpp = pkgs.callPackage ./nix/build.nix { };
-        packages.default = self.packages.${system}.pprintpp;
-        checks = {
-          clang-build = self.packages.${system}.pprintpp.override {
-            stdenv = pkgs.clangStdenv;
-          };
-          gcc-build = self.packages.${system}.pprintpp.override {
-            stdenv = pkgs.clangStdenv;
-          };
-          example-standalone = pkgs.stdenv.mkDerivation {
-            name = "pprintpp-example-project";
-            src = ./example;
-            buildInputs = [ self.packages.${system}.pprintpp ];
-            nativeBuildInputs = [ pkgs.cmake ];
+        systems = [ "x86_64-linux" ];
+        perSystem = { config, pkgs, system, ... }: {
+          packages.pprintpp = pkgs.callPackage ./nix/build.nix { };
+          packages.default = config.packages.pprintpp;
+          checks = {
+            clang-build = config.packages.pprintpp.override {
+              stdenv = pkgs.clangStdenv;
+            };
+            gcc-build = config.packages.pprintpp.override {
+              stdenv = pkgs.clangStdenv;
+            };
+            example-standalone = pkgs.stdenv.mkDerivation {
+              name = "pprintpp-example-project";
+              src = ./example;
+              buildInputs = [ config.packages.pprintpp ];
+              nativeBuildInputs = [ pkgs.cmake ];
+            };
           };
         };
-      }
-    )) // {
+      } // {
       overlays.default = final: prev: {
         pprintpp = final.callPackage ./nix/build.nix { };
       };
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,3 +1,3 @@
 self: super: {
-  pprintpp = self.callPackage ./build.nix {};
+  pprintpp = self.callPackage ./build.nix { };
 }
