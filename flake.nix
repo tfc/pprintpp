55bfc9ea2584471ad247648b39c62514439e5a24
Jacek Galowicz <jacek@galowicz.de>
2023-08-07 09:39:16 +0200

    Modernize flake

--- flake.nix
+++ flake.nix
@@ -4,46 +4,46 @@
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
-  outputs = { self, flake-parts, nixpkgs, pre-commit-hooks }:
-    flake-parts.lib.mkFlake { inherit self; }
-      {
-        systems = [ "x86_64-linux" ];
-        perSystem = { config, pkgs, system, ... }: {
-          packages.pprintpp = pkgs.callPackage ./nix/build.nix { };
-          packages.default = config.packages.pprintpp;
 
-          devShells.default = pkgs.mkShell {
-            inputsFor = [ config.packages.pprintpp ];
-            shellHook = ''
-              ${config.checks.pre-commit-check.shellHook}
-            '';
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; }
+    {
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, pkgs, system, ... }: {
+        packages.pprintpp = pkgs.callPackage ./nix/build.nix { };
+        packages.default = config.packages.pprintpp;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ config.packages.pprintpp ];
+          shellHook = ''
+            ${config.checks.pre-commit-check.shellHook}
+          '';
+        };
+        checks = {
+          clang-build = config.packages.pprintpp.override {
+            stdenv = pkgs.clangStdenv;
           };
-          checks = {
-            clang-build = config.packages.pprintpp.override {
-              stdenv = pkgs.clangStdenv;
-            };
-            gcc-build = config.packages.pprintpp.override {
-              stdenv = pkgs.clangStdenv;
-            };
-            example-standalone = pkgs.stdenv.mkDerivation {
-              name = "pprintpp-example-project";
-              src = ./example;
-              buildInputs = [ config.packages.pprintpp ];
-              nativeBuildInputs = [ pkgs.cmake ];
-            };
-            pre-commit-check = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                clang-format.enable = true;
-                nixpkgs-fmt.enable = true;
-                statix.enable = true;
-              };
+          gcc-build = config.packages.pprintpp.override {
+            stdenv = pkgs.clangStdenv;
+          };
+          example-standalone = pkgs.stdenv.mkDerivation {
+            name = "pprintpp-example-project";
+            src = ./example;
+            buildInputs = [ config.packages.pprintpp ];
+            nativeBuildInputs = [ pkgs.cmake ];
+          };
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              clang-format.enable = true;
+              nixpkgs-fmt.enable = true;
+              statix.enable = true;
             };
           };
         };
-      } // {
-      overlays.default = final: prev: {
-        pprintpp = final.callPackage ./nix/build.nix { };
       };
+    } // {
+    overlays.default = final: prev: {
+      pprintpp = final.callPackage ./nix/build.nix { };
     };
+  };
 }
